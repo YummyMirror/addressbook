36da06b84637981663667a408eda6ccadb216c42
YummyMirror <anatolianiukevich@gmail.com>
2017-07-01 00:57:10 +0300

    Have created 'BeforeMethod' with 'ensurePrecondition' method for GroupCreationTests class

--- addressbook/src/test/java/ru/anatoli/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/tests/GroupCreationTests.java
@@ -1,23 +1,26 @@
 package ru.anatoli.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.anatoli.addressbook.models.GroupData;
 import java.util.Set;
 import static org.testng.Assert.assertEquals;
 
 public class GroupCreationTests extends TestBase {
+    @BeforeMethod
+    public void ensurePrecondition() {
+        applicationManager.getNavigationHelper().goToGroupsPage();
+    }
+    
     @Test
     public void testGroupCreation() {
-        GroupData groupData = new GroupData().withGroupName("GroupName")
-                                            .withGroupHeader("GroupHeader")
-                                            .withGroupFooter(null);
-
-        applicationManager.getNavigationHelper().goToGroupsPage();
-
         //Getting Set of GroupData object model BEFORE creation
         Set<GroupData> before = applicationManager.getGroupHelper().getGroupsHash();
 
         //Creating new GROUP
+        GroupData groupData = new GroupData().withGroupName("GroupName")
+                                            .withGroupHeader("GroupHeader")
+                                            .withGroupFooter(null);
         applicationManager.getGroupHelper().createGroup(groupData);
 
         //Getting Set of GroupData object model AFTER creation
