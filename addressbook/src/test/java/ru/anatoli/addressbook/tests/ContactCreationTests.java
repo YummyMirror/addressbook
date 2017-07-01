8c1b576160da1e5f9390437f74686370c763af79
YummyMirror <anatolianiukevich@gmail.com>
2017-07-01 21:10:50 +0300

    Have added 'ensurePrecondition' method for 'testContactCreation'

--- addressbook/src/test/java/ru/anatoli/addressbook/tests/ContactCreationTests.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/tests/ContactCreationTests.java
@@ -1,5 +1,6 @@
 package ru.anatoli.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.anatoli.addressbook.models.ContactData;
 import java.util.Set;
@@ -9,10 +10,13 @@ import static org.testng.Assert.assertEquals;
  * Created by anatoli.anukevich on 6/27/2017.
  */
 public class ContactCreationTests extends TestBase {
+    @BeforeMethod
+    public void ensurePrecondition() {
+        applicationManager.getNavigationHelper().goToHomePage();
+    }
+
     @Test
     public void testContactCreation() {
-        applicationManager.getNavigationHelper().goToHomePage();
-
         //Getting Set of ContactData object model BEFORE creation
         Set<ContactData> before = applicationManager.getContactHelper().getContactSet();
 
