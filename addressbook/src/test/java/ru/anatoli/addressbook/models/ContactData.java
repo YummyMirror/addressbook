c3f812e14b6a53af37b237c638ad0b905d3a00eb
YummyMirror <anatolianiukevich@gmail.com>
2017-07-11 12:57:07 +0300

    Have added new fields 'homePhone', 'mobilePhone', 'workPhone', 'fax', 'email', 'email2', 'email3' and 'homepage' for ContactData object model. Have created getters and setters for new fields

--- addressbook/src/test/java/ru/anatoli/addressbook/models/ContactData.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/models/ContactData.java
@@ -12,6 +12,14 @@ public class ContactData {
     private String title;
     private String company;
     private String address;
+    private String homePhone;
+    private String mobilePhone;
+    private String workPhone;
+    private String fax;
+    private String email;
+    private String email2;
+    private String email3;
+    private String homepage;
 
     //Getters
     public int getContactId() {
@@ -46,8 +54,39 @@ public class ContactData {
         return address;
     }
 
-    //Setters
+    public String getHomePhone() {
+        return homePhone;
+    }
 
+    public String getMobilePhone() {
+        return mobilePhone;
+    }
+
+    public String getWorkPhone() {
+        return workPhone;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getEmail2() {
+        return email2;
+    }
+
+    public String getEmail3() {
+        return email3;
+    }
+
+    public String getHomepage() {
+        return homepage;
+    }
+
+    //Setters
     public ContactData withContactId(int id) {
         this.id = id;
         return this;
@@ -85,6 +124,46 @@ public class ContactData {
 
     public ContactData withAddress(String address) {
         this.address = address;
+        return this;
+    }
+
+    public ContactData withHomePhone(String homePhone) {
+        this.homePhone = homePhone;
+        return this;
+    }
+
+    public ContactData withMobilePhone(String mobilePhone) {
+        this.mobilePhone = mobilePhone;
+        return this;
+    }
+
+    public ContactData withWorkPhone(String workPhone) {
+        this.workPhone = workPhone;
+        return this;
+    }
+
+    public ContactData withFax(String fax) {
+        this.fax = fax;
+        return  this;
+    }
+
+    public ContactData withEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public ContactData withEmail2(String email2) {
+        this.email2 = email2;
+        return this;
+    }
+
+    public ContactData withEmail3(String email3) {
+        this.email3 = email3;
+        return this;
+    }
+
+    public ContactData withHomepage(String homepage) {
+        this.homepage = homepage;
         return this;
     }
 
