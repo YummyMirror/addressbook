ca1ecde38818a3febfcab2b7dc3e8d1bcc8eccab
YummyMirror <anatolianiukevich@gmail.com>
2017-07-13 10:52:39 +0300

    Have added 'ensurePrecondition' method for 'testGroupAllDeletion' test

--- addressbook/src/test/java/ru/anatoli/addressbook/tests/GroupAllDeletionTests.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/tests/GroupAllDeletionTests.java
@@ -1,5 +1,6 @@
 package ru.anatoli.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.anatoli.addressbook.models.GroupData;
 import java.util.Set;
@@ -8,10 +9,21 @@ import java.util.Set;
  * Created by anatoli.anukevich on 7/13/2017.
  */
 public class GroupAllDeletionTests extends TestBase {
+    @BeforeMethod
+    public void ensurePrecondition() {
+        applicationManager.getNavigationHelper().goToGroupsPage();
+        if (applicationManager.getGroupHelper().getGroupSet().size() == 0) {
+            GroupData groupData = new GroupData().withGroupName("Test name")
+                                                .withGroupHeader(null)
+                                                .withGroupFooter(null);
+            applicationManager.getGroupHelper().createGroup(groupData);
+            applicationManager.getGroupHelper().createGroup(groupData);
+            applicationManager.getGroupHelper().createGroup(groupData);
+        }
+    }
+
     @Test(enabled = true)
     public void testGroupAllDeletion() {
-        applicationManager.getNavigationHelper().goToGroupsPage();
-
         //Getting Set of GroupData object model BEFORE deletion
         Set<GroupData> before = applicationManager.getGroupHelper().getGroupSet();
 
