b0c10747df1bf42b010a66e77955736a647aa97a
YummyMirror <anatolianiukevich@gmail.com>
2017-07-15 16:15:06 +0300

    Have added algoritm with checking 'filled in/empty' emails to the assertion by Emails to the 'testContactCompOutsideInsideUpdate' test

--- addressbook/src/test/java/ru/anatoli/addressbook/tests/ContactCompOutsideInsideEditTests.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/tests/ContactCompOutsideInsideEditTests.java
@@ -31,13 +31,60 @@ public class ContactCompOutsideInsideEditTests extends TestBase {
         //Asserting by Address
         assertEquals(outsideData.getAddress(), insideData.getAddress());
 
+        //Getting INSIDE's emails
+        String insideEmail1 = insideData.getEmail();
+        String insideEmail2 = insideData.getEmail2();
+        String insideEmail3 = insideData.getEmail3();
+
         //Asserting by Emails
-        assertEquals(outsideData.getAllEmails(), mergeInsideEmails(insideData.getEmail(), insideData.getEmail2(), insideData.getEmail3()));
+        if (insideEmail1.equals("") && !insideEmail2.equals("") && !insideEmail3.equals("")) {
+            assertEquals(outsideData.getAllEmails(), mergeSecondThirdEmails(insideEmail2, insideEmail3));
+        } else if (insideEmail2.equals("") && !insideEmail1.equals("") && !insideEmail3.equals("")) {
+            assertEquals(outsideData.getAllEmails(), mergeFirstThirdEmails(insideEmail1, insideEmail3));
+        } else if (insideEmail3.equals("") && !insideEmail1.equals("") && !insideEmail2.equals("")) {
+            assertEquals(outsideData.getAllEmails(), mergeFirstSecondEmails(insideEmail1, insideEmail2));
+        } else if (insideEmail1.equals("") && insideEmail3.equals("") && !insideEmail2.equals("")) {
+            assertEquals(outsideData.getAllEmails(), insideEmail2);
+        } else if (insideEmail1.equals("") && insideEmail2.equals("") && !insideEmail3.equals("")) {
+            assertEquals(outsideData.getAllEmails(), insideEmail3);
+        } else if (insideEmail2.equals("") && insideEmail3.equals("") && !insideEmail1.equals("")) {
+            assertEquals(outsideData.getAllEmails(), insideEmail1);
+        } else if (!insideEmail1.equals("") && !insideEmail2.equals("") && !insideEmail3.equals("")) {
+            assertEquals(outsideData.getAllEmails(), mergeAllInsideEmails(insideEmail1, insideEmail2, insideEmail3));
+        } else if (insideEmail1.equals("") && insideEmail2.equals("") && insideEmail3.equals("")) {
+        assertEquals(outsideData.getAllEmails(), mergeAllEmptyEmails(insideEmail1, insideEmail2, insideEmail3));
+        } else {
+            System.out.println("ERROR with conditions occurs. Investigation is needed!!!");
+        }
     }
 
-    public String mergeInsideEmails(String email, String email2, String email3) {
+    public String mergeAllInsideEmails(String email, String email2, String email3) {
         String mergedEmails = email + " " + email2 + " " + email3;
         String replaced = mergedEmails.replaceAll(" ", "\n");
+        return replaced;
+    }
+
+    public String mergeFirstSecondEmails(String email, String email2) {
+        String mergedEmails = email + " " + email2;
+        String replaced = mergedEmails.replaceAll(" ", "\n");
+        return replaced;
+    }
+
+    public String mergeFirstThirdEmails(String email, String email3) {
+        String mergedEmails = email + " " + email3;
+        String replaced = mergedEmails.replaceAll(" ", "\n");
+        return replaced;
+    }
+
+    public String mergeSecondThirdEmails(String email2, String email3) {
+        String mergedEmails = email2 + " " + email3;
+        String replaced = mergedEmails.replaceAll(" ", "\n");
+        return replaced;
+    }
+
+    public String mergeAllEmptyEmails(String email, String email2, String email3) {
+        String mergedEmails = email + " " + email2 + " " + email3;
+        String replaced = mergedEmails.replaceAll(" ", "");
         return replaced;
     }
 }
