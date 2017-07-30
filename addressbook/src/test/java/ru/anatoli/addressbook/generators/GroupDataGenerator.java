b373f09242de2eb81fd142b02a4749d018407b52
YummyMirror <anatolianiukevich@gmail.com>
2017-07-30 20:54:33 +0300

    Have created the 'saveAsCsv' method for the 'GroupDataGenerator' class

--- addressbook/src/test/java/ru/anatoli/addressbook/generators/GroupDataGenerator.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/generators/GroupDataGenerator.java
@@ -2,21 +2,42 @@ package ru.anatoli.addressbook.generators;
 
 import ru.anatoli.addressbook.models.GroupData;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UnknownFormatConversionException;
 
 /**
  * Created by anatoli.anukevich on 7/30/2017.
  */
 public class GroupDataGenerator {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         int numberOfGroups = Integer.parseInt(args[0]);
         String pathToFile = args[1];
         String fileName = args[2];
         String format = args[3];
-        File file = new File(pathToFile + fileName);
+        File file = new File(pathToFile + fileName + "." + format);
 
         List<GroupData> listWithGroups = generateGroups(numberOfGroups);
+
+        if (format.equals("csv")) {
+            saveAsCsv(file, listWithGroups);
+        } else if (format.equals("json")) {
+
+        } else {
+            System.out.println("Unknown format is input " + format);
+            throw new UnknownFormatConversionException("Unknown format is input " + format);
+        }
+    }
+
+    public static void saveAsCsv(File file, List<GroupData> listWithGroups) throws IOException {
+        FileWriter writer = new FileWriter(file);
+        for (GroupData group : listWithGroups) {
+            writer.write(String.format("%s;%s;%s\n", group.getGroupName(), group.getGroupHeader(), group.getGroupFooter()));
+        }
+        writer.flush();
+        writer.close();
     }
 
     public static List<GroupData> generateGroups(int numberOfGroups) {
