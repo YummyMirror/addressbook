b4821deb3dfedae3e8ee64226bb7deec8a139b61
YummyMirror <anatolianiukevich@gmail.com>
2017-07-31 18:46:58 +0300

    Have created the DataProvider from 'CSV' file for the 'testGroupCreation' test

--- addressbook/src/test/java/ru/anatoli/addressbook/tests/GroupCreationTests.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/tests/GroupCreationTests.java
@@ -1,26 +1,44 @@
 package ru.anatoli.addressbook.tests;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import ru.anatoli.addressbook.models.GroupData;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
 import static org.testng.Assert.assertEquals;
 
 public class GroupCreationTests extends TestBase {
+    @DataProvider
+    public Iterator<Object[]> validDataForGroupCreationFromCsv() throws IOException {
+        List<Object[]> list = new ArrayList<Object[]>();
+        File file = new File("src/test/resources/groupFile.csv");
+        FileReader reader = new FileReader(file);
+        BufferedReader bufferedReader = new BufferedReader(reader);
+        String line = bufferedReader.readLine();
+        while (line != null) {
+            String[] splitData = line.split(";");
+            list.add(new Object[] {new GroupData().withGroupName(splitData[0])
+                                                    .withGroupHeader(splitData[1])
+                                                    .withGroupFooter(splitData[2])});
+            line = bufferedReader.readLine();
+        }
+        return list.iterator();
+    }
     @BeforeMethod
     public void ensurePrecondition() {
         applicationManager.getNavigationHelper().goToGroupsPage();
     }
     
-    @Test
-    public void testGroupCreation() {
+    @Test(dataProvider = "validDataForGroupCreationFromCsv")
+    public void testGroupCreation(GroupData groupData) {
         //Getting Set of GroupData object model BEFORE creation
         Set<GroupData> before = applicationManager.getGroupHelper().getGroupSet();
 
-        //Creating new GROUP
-        GroupData groupData = new GroupData().withGroupName("GroupName")
-                                            .withGroupHeader("GroupHeader")
-                                            .withGroupFooter(null);
+        //Creating new GROUPs
         applicationManager.getGroupHelper().createGroup(groupData);
 
         //Getting Set of GroupData object model AFTER creation
