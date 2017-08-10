f6e8eac30b0da10e448e82ecb0f06cf7f8eea074
YummyMirror <anatolianiukevich@gmail.com>
2017-08-10 11:30:16 +0300

    Have created 'DbHelper' class and connected to the DN in the Constructor

--- addressbook/src/test/java/ru/anatoli/addressbook/appmanager/DbHelper.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/appmanager/DbHelper.java
@@ -1,12 +1,22 @@
 package ru.anatoli.addressbook.appmanager;
 
-import org.openqa.selenium.WebDriver;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 
 /**
  * Created by anatoli.anukevich on 8/10/2017.
  */
-public class DbHelper extends HelperBase {
-    public DbHelper(WebDriver wd) {
-        super(wd);
+public class DbHelper {
+    private SessionFactory sessionFactory;
+
+    public DbHelper() {
+        // A SessionFactory is set up once for an application!
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                                                                                    .configure() // configures settings from hibernate.cfg.xml
+                                                                                    .build();
+        sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
     }
 }
