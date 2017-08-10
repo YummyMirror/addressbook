2a154a9c551fa5998a0dea3c4af4b9bdc03dc961
YummyMirror <anatolianiukevich@gmail.com>
2017-08-10 11:16:39 +0300

    Have created the 'hibernate.cfg.xml' file for connection with DB

---
+++ addressbook/src/test/java/ru/anatoli/addressbook/appmanager/DbHelper.java
@@ -0,0 +1,12 @@
+package ru.anatoli.addressbook.appmanager;
+
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created by anatoli.anukevich on 8/10/2017.
+ */
+public class DbHelper extends HelperBase {
+    public DbHelper(WebDriver wd) {
+        super(wd);
+    }
+}
