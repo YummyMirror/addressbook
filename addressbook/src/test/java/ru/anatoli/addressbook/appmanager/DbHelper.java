fed9308dca8d140c7057ebb4c9e86814a5ca489d
YummyMirror <anatolianiukevich@gmail.com>
2017-08-11 10:28:11 +0300

    Have changed the type of returning collection from 'ArrayList' to the 'HashSet' in both 'getGroupSet' and 'getContactSet' methods in the 'DbHelper'

--- addressbook/src/test/java/ru/anatoli/addressbook/appmanager/DbHelper.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/appmanager/DbHelper.java
@@ -1,6 +1,5 @@
 package ru.anatoli.addressbook.appmanager;
 
-
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
@@ -8,7 +7,9 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.anatoli.addressbook.models.ContactData;
 import ru.anatoli.addressbook.models.GroupData;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Created by anatoli.anukevich on 8/10/2017.
@@ -25,21 +26,21 @@ public class DbHelper {
         sessionFactory = new MetadataSources( registry ).buildMetadata().buildSessionFactory();
     }
 
-    public List<GroupData> getGroupList() {
+    public Set<GroupData> getGroupSet() {
         session = sessionFactory.openSession();
         session.beginTransaction();
         List<GroupData> list = session.createQuery( "from GroupData" ).list();
         session.getTransaction().commit();
         session.close();
-        return list;
+        return new HashSet<GroupData>(list);
     }
 
-    public List<ContactData> getContactList() {
+    public Set<ContactData> getContactSet() {
         session = sessionFactory.openSession();
         session.beginTransaction();
         List<ContactData> list = session.createQuery( "from ContactData WHERE deprecated = '0000-00-00 00:00:00'" ).list();
         session.getTransaction().commit();
         session.close();
-        return list;
+        return new HashSet<ContactData>(list);
     }
 }
