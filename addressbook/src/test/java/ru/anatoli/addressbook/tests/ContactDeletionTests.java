c83d5a3697382f0f1e6bfb8c80f21f6d016dcffc
YummyMirror <anatolianiukevich@gmail.com>
2017-08-20 10:07:01 +0300

    Have added assertion UI data vs. DB data for the 'testContactDeletion' test

--- addressbook/src/test/java/ru/anatoli/addressbook/tests/ContactDeletionTests.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/tests/ContactDeletionTests.java
@@ -5,6 +5,7 @@ import org.testng.annotations.Test;
 import ru.anatoli.addressbook.models.ContactData;
 import java.io.File;
 import java.util.Set;
+import java.util.stream.Collectors;
 import static org.testng.Assert.assertEquals;
 
 /**
@@ -64,6 +65,21 @@ public class ContactDeletionTests extends TestBase {
 
         //Asserting by COLLECTIONS
         assertEquals(before, after);
+
+        //Asserting UI data vs DB data
+        Set<ContactData> ui = applicationManager.getContactHelper().getContactSet().stream()
+                                                                                   .map((contact) -> new ContactData().withContactId(contact.getContactId())
+                                                                                                                      .withFirstName(contact.getFirstName())
+                                                                                                                      .withLastName(contact.getLastName())
+                                                                                                                      .withAddress(contact.getAddress()))
+                                                                                   .collect(Collectors.toSet());
+        Set<ContactData> db = applicationManager.getDbHelper().getContactSet().stream()
+                                                                              .map((contact) -> new ContactData().withContactId(contact.getContactId())
+                                                                                                                 .withFirstName(contact.getFirstName())
+                                                                                                                 .withLastName(contact.getLastName())
+                                                                                                                 .withAddress(contact.getAddress()))
+                                                                              .collect(Collectors.toSet());
+        assertEquals(ui, db);
     }
 
     @Test(enabled = true)
