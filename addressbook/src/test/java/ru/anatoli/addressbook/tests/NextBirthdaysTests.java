ec47c46b0f561ee3e2ae4f1451fae8dd97382c15
YummyMirror <anatolianiukevich@gmail.com>
2017-08-25 11:15:27 +0300

    Have added 'ensurePrecondition' method to the 'testNextBirthdays' test

--- addressbook/src/test/java/ru/anatoli/addressbook/tests/NextBirthdaysTests.java
+++ addressbook/src/test/java/ru/anatoli/addressbook/tests/NextBirthdaysTests.java
@@ -1,7 +1,9 @@
 package ru.anatoli.addressbook.tests;
 
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.anatoli.addressbook.models.ContactData;
+import java.io.File;
 import java.util.Set;
 import static org.testng.Assert.assertEquals;
 
@@ -9,10 +11,41 @@ import static org.testng.Assert.assertEquals;
  * Created by anatoli.anukevich on 8/24/2017.
  */
 public class NextBirthdaysTests extends TestBase {
+    @BeforeMethod
+    public void ensurePrecondition() {
+        if (applicationManager.getDbHelper().getContactSet().size() == 0) {
+            ContactData contactData = new ContactData().withFirstName("Temp first name")
+                                                        .withMiddleName(null)
+                                                        .withLastName("")
+                                                        .withNickname(null)
+                                                        .withPhoto(new File(""))
+                                                        .withTitle(null)
+                                                        .withCompany(null)
+                                                        .withAddress(null)
+                                                        .withHomePhone(null)
+                                                        .withMobilePhone(null)
+                                                        .withWorkPhone(null)
+                                                        .withFax(null)
+                                                        .withEmail(null)
+                                                        .withEmail2(null)
+                                                        .withEmail3(null)
+                                                        .withHomepage(null)
+                                                        .withBirthDay("1")
+                                                        .withBirthMonth("September")
+                                                        .withBirthYear(null)
+                                                        .withAnniversaryDay(null)
+                                                        .withAnniversaryMonth(null)
+                                                        .withAnniversaryYear(null)
+                                                        .withSecondaryAddress(null)
+                                                        .withSecondaryPhone(null)
+                                                        .withSecondaryNotes(null);
+            applicationManager.getContactHelper().createContact(contactData);
+        }
+        applicationManager.getNavigationHelper().goToNextBirthdaysPage();
+    }
+
     @Test(enabled = true)
     public void testNextBirthdays() {
-        applicationManager.getNavigationHelper().goToNextBirthdaysPage();
-
         //Getting Set of ContactData object model from DataBase with specified BirthDays and BirthMonths values
         Set<ContactData> contactSetWithBirthdays = applicationManager.getDbHelper().getContactSetWithBirthdays();
 
